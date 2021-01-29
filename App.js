9d054f6e6ad460f539d2f2a1838ed4a86755e531
Dina <dmuhic@gmail.com>
2021-01-29 18:40:26 +0100

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Home = () => {
+  return (
+  <View style={styles.container}>
+      <Text>SmarTask!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+function RootStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ gestureEnabled: false }}
+    >
+      <Stack.Screen name="Home"
+      component={Home}
+      options={{ title: "SmarTasking" }}
+      />
+      <Stack.Screen name="Details"
+      component={AddDetails}
+      options={{ title: "Add Details" }}
+      />
+      <Stack.Screen name="Edit"
+      component={editDetails}
+      options={{ title: "Edit Task" }}
+      />
+      <Stack.Screen name="List"
+      component={ListPage}
+      options={{ title: "Tasks" }}
+      />
+      <Stack.Screen name="Schedule"
+      component={ScheduleView}
+      options={{ title: "Today's Tasks" }}
+      />
+
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+   
   );
 }
 
