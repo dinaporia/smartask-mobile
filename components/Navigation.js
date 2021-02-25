eadb9a71bc736246d91dda457e14cab2247894bc
Dina <dmuhic@gmail.com>
2021-02-25 12:45:15 +0100

    remove stack navigator, add header menu bar

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Icon } from "react-native-elements";
 
 import Home from "./Home";
 import ListPage from "./ListScreen/ListPage";
@@ -8,11 +8,9 @@ import ScheduleView from "./ScheduleScreen/ScheduleView";
 import AddTaskPage from "./AddTaskScreen/AddTaskPage";
 import Preferences from "./PreferencesScreen";
 
-const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const screenOptionStyle = {
-  gestureEnabled: false,
   headerStyle: {
     backgroundColor: "#2a363b",
   },
@@ -21,107 +19,59 @@ const screenOptionStyle = {
     textShadowRadius: 5,
     color: "white",
   },
-};
-
-const HomeNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={screenOptionStyle} initialRouteName="Home">
-      <Stack.Screen name="SmartTask" component={Home} />
-      <Stack.Screen
-        name="Add"
-        component={AddTaskPage}
-        options={{ title: "Add a task" }}
-      />
-      <Stack.Screen
-        name="Schedule"
-        component={ScheduleView}
-        options={{ title: "Today's Tasks" }}
-      />
-      <Stack.Screen
-        name="List"
-        component={ListPage}
-        options={{ title: "Task List" }}
-      />
-      <Stack.Screen
-        name="Prefs"
-        component={Preferences}
-        options={{ title: "Preferences" }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const TaskNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen
-        name="Add"
-        component={AddTaskPage}
-        options={{ title: "Add a task" }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const ScheduleNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen
-        name="Schedule"
-        component={ScheduleView}
-        options={{ title: "Today's Tasks" }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const ListNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={screenOptionStyle} defaultNavigationOptions>
-      <Stack.Screen
-        name="List"
-        component={ListPage}
-        options={{ title: "Task List" }}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const PrefsNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen
-        name="Prefs"
-        component={Preferences}
-        options={{ title: "Preferences" }}
-      />
-    </Stack.Navigator>
-  );
+  headerTitleAlign: 'center',
+  headerShown: true
 };
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeNavigator} />
+    <Drawer.Navigator initialRouteName="Home"
+      screenOptions={({ navigation }) => ({
+        headerLeft: () => (
+          <Icon 
+            name='bars'
+            type='font-awesome'
+            color='white'
+            size={30}
+            iconStyle={{paddingHorizontal: 20}}
+            onPress={() => navigation.openDrawer()}
+          />
+        )
+      })} 
+    >
+      <Drawer.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          gestureEnabled: false
+        }}
+      />
       <Drawer.Screen
         name="Add"
-        component={TaskNavigator}
-        options={{ title: "Add a task" }}
+        component={AddTaskPage}
+        options={{ 
+          ...screenOptionStyle,
+          title: "Add a task",
+           }}
       />
       <Drawer.Screen
         name="List"
-        component={ListNavigator}
-        options={{ title: "Task List" }}
+        component={ListPage}
+        options={{ 
+          ...screenOptionStyle,
+          title: "Task List" }}
       />
       <Drawer.Screen
         name="Schedule"
-        component={ScheduleNavigator}
-        options={{ title: "Today's Tasks" }}
+        component={ScheduleView}
+        options={{  ...screenOptionStyle,
+        title: "Today's Tasks" }}
       />
       <Drawer.Screen
         name="Prefs"
-        component={PrefsNavigator}
-        options={{ title: "Preferences" }}
+        component={Preferences}
+        options={{  ...screenOptionStyle,
+        title: "Settings" }}
       />
     </Drawer.Navigator>
   );
