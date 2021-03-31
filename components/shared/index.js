0a4737ab1fb89550f0ede5c1e393e67bd1b71228
Dina <dmuhic@gmail.com>
2021-03-31 21:00:32 +0200

    description added

--- components/shared/index.js
+++ components/shared/index.js
@@ -2,6 +2,6 @@ import Rating from './Rating/TapRating';
 import RenderTaskList from './RenderTaskList';
 import { TaskNameInput, TaskCategoryInput, TaskDateInput } from './TaskBasics';
 import { DurationInput, DifficultyInput, InterestInput, PriorityInput } from './TaskDetails';
-import Footer, { TaskCounter } from './Footer';
+import Footer from './Footer';
 
-export { Rating, RenderTaskList, TaskNameInput, TaskCategoryInput, TaskDateInput, DurationInput, DifficultyInput, InterestInput, PriorityInput, TaskCounter, Footer };
+export { Rating, RenderTaskList, TaskNameInput, TaskCategoryInput, TaskDateInput, DurationInput, DifficultyInput, InterestInput, PriorityInput, Footer };
